8b858d82663b9f8cb783d1a0f38cab80ce1726d0
LIMDANBI <dlaeksql08222@gmail.com>
2024-04-21 13:50:15 +0900

    static member

--- 2_Object Oriented Programming/5_static_member.cpp
+++ 2_Object Oriented Programming/5_static_member.cpp
@@ -12,10 +12,37 @@ public:
 };
 int Car::count{0}; // = 0
 
+
+class Object
+{
+	int idata;
+	static int sdata;
+public:
+	void ifunc()
+	{
+		idata = 10;	// 1. ok
+		sdata = 10;	// 2. ok
+		sfunc();	// 3. ok
+	}
+	static void sfunc()
+	{
+		idata = 10;	// 4. error
+		sdata = 10;	// 5. ok
+		ifunc();	// 6. error
+	}
+};
+int Object::sdata{0};
+
+
 int main()
 {
 	Car c1;
 	Car c2;
 
-	std::cout << Car::count << std::endl; 							
+	std::cout << Car::count << std::endl;
+	
+	Object::sfunc();
+
+	Object obj;
+	obj.ifunc();					
 }
